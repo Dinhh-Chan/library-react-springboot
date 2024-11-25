d644865b2516c3f8098aa7e1753dbe335f6dfde8
Dinhh-Chan <dinhtran29092005@gmail.com>
2024-11-25 15:57:16 +0700

    update navbar

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,30 +1,36 @@
-import React from "react";
-import { BrowserRouter as Router, Route, NavLink, Link } from 'react-router-dom'
-import './NavBar.css'
+// src/components/NavBar/NavBar.js
+
+import React, { useState } from "react";
+import { Link } from 'react-router-dom'; // Import Link từ react-router-dom
+import './NavBar.css';
 import SearchBar from "../SearchBar/SearchBar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import DropDownMenu from "../DropDownItems/DropDownItems";
-import { useState } from "react";
-
 
 function NavBar() {
-    const [openMenu,setOpenMenu] = useState(false);
-
+    const [openMenu, setOpenMenu] = useState(false);
 
     return ( 
         <>
-        <nav className="navbar">
-                <img src='https://i.imgur.com/YVydVYH.png' alt="https://i.imgur.com/YVydVYH.png"></img> {/*thay img cho logo*/}
-            <SearchBar></SearchBar>
-            <button className="menuButton" onClick={() => setOpenMenu(!openMenu)}>
-                <FontAwesomeIcon icon={faBars} />
-            </button>
-        </nav>
-        {openMenu && <DropDownMenu></DropDownMenu>}
-        <div style={{height:"80px"}}></div>
+            <nav className="navbar">
+                {/* Bao bọc img bằng Link để chuyển hướng về "/" */}
+                <Link to="/" className="navbar-logo">
+                    <img 
+                        src='https://i.imgur.com/YVydVYH.png' 
+                        alt="Logo" // Sử dụng alt mô tả rõ ràng hơn
+                    />
+                </Link>
+                <SearchBar />
+                <button className="menuButton" onClick={() => setOpenMenu(!openMenu)}>
+                    <FontAwesomeIcon icon={faBars} />
+                </button>
+            </nav>
+            {openMenu && <DropDownMenu />}
+            {/* Thêm khoảng trống dưới navbar */}
+            <div style={{height:"80px"}}></div>
         </>
-     );
+    );
 }
 
 export default NavBar;
